0b8ce73f37b43a8e8d7abafc4c6ca21169e14f12
ange <forever_youyou@foxmail.com>
2024-01-29 14:26:36 +0800

    goravel

--- internal/project/project_goravel.go
+++ internal/project/project_goravel.go
@@ -1,7 +1,6 @@
 package project
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,13 +11,18 @@ import (
 	"github.com/yafgo/yafgo/internal/pkg/file"
 )
 
-type TplGoravel struct {
-	repo string
+func NewTplGoravel() *TplGoravel {
+	tpl := &TplGoravel{
+		TplBase: TplBase{
+			REPO_GITHUB: "https://github.com/goravel/goravel.git",
+		},
+	}
+	return tpl
 }
 
-const (
-	GORAVEL_REPO_GITHUB = "https://github.com/goravel/goravel.git"
-)
+type TplGoravel struct {
+	TplBase
+}
 
 func (rp *TplGoravel) Name() string {
 	return "Goravel"
@@ -30,61 +34,46 @@ func (rp *TplGoravel) Name() string {
 func (rp *TplGoravel) MakeProject(name string) (err error) {
 
 	// set repo
-	rp.repo = GORAVEL_REPO_GITHUB
-
-	// eg: "project" "my_project"
-	projectName := path.Base(name)
-	// workDir
-	cwd, _ := os.Getwd()
-	projectDir := path.Join(cwd, projectName)
+	rp.repo = rp.REPO_GITHUB
 
 	defer func() {
 		if err != nil {
 			os.Exit(1)
 		}
 
-		fmt.Println(color.Success.Sprint("创建成功: "), projectName)
+		fmt.Println(color.Success.Sprint("创建成功: "), rp.dirName)
 	}()
 
-	if file.Exists(projectDir) {
-		fmt.Println(color.Warn.Sprint("项目已存在: "), projectName)
-		err = errors.New("project already exists")
+	// 初始化项目信息
+	if err = rp.initProjectDir(name); err != nil {
 		return
 	}
 
 	// clone project
-	{
-		fmt.Println(color.Gray.Sprint("创建项目: "), projectName)
-		cmd := exec.Command("git", "clone", rp.repo, projectName)
-		color.Grayln(cmd.String())
-		err = cmd.Run()
-		if err != nil {
-			color.Redf("创建失败, 请检查 [%v] 访问状况并重试!\n", rp.repo)
-			return
-		}
+	if err = rp.cloneProject(); err != nil {
+		return
 	}
 
 	// delete .git
-	_ = os.RemoveAll(path.Join(projectDir, ".git"))
+	_ = os.RemoveAll(path.Join(rp.projectDir, ".git"))
 
 	// replace moduleName
-	rp.renameModule(name, projectDir)
+	rp.renameModule(rp.projectDir)
 
 	// chdir
-	if _err := os.Chdir(projectDir); _err != nil {
+	if _err := os.Chdir(rp.projectDir); _err != nil {
 		err = _err
 		return
 	}
 
 	// gofmt
-	{
-		cmd := exec.Command("gofmt", "-w", ".")
-		color.Grayln(cmd.String())
-		err = cmd.Run()
-		if err != nil {
-			color.Errorln("gofmt fail")
-			return
-		}
+	if err = rp.runGoFmt(); err != nil {
+		return
+	}
+
+	// git init
+	if err = rp.runGitInit(); err != nil {
+		return
 	}
 
 	// create .env
@@ -98,31 +87,20 @@ func (rp *TplGoravel) MakeProject(name string) (err error) {
 		}
 	}
 
-	// git init
-	{
-		cmd := exec.Command("git", "init")
-		color.Grayln(cmd.String())
-		err = cmd.Run()
-		if err != nil {
-			color.Errorln("git init fail")
-			return
-		}
-	}
-
 	return
 }
 
-func (rp *TplGoravel) renameModule(moduleName, dir string) (err error) {
+func (rp *TplGoravel) renameModule(dir string) (err error) {
 
 	err = file.WalkFiles(dir, func(elem string) error {
 		// go.mod
 		if path.Base(elem) == "go.mod" {
-			err := file.ReplaceString(elem, "module goravel", "module "+moduleName)
+			err := file.ReplaceString(elem, "module goravel", "module "+rp.moduleName)
 			return err
 		}
 		// *.go
 		if strings.HasSuffix(elem, ".go") {
-			err := file.ReplaceString(elem, `"goravel/`, `"`+moduleName+`/`)
+			err := file.ReplaceString(elem, `"goravel/`, `"`+rp.moduleName+`/`)
 			return err
 		}
 
